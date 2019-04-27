2d71db9068bb9c641dd4ed75f00a9f76e0244d1e
Senduran <s3604069@student.rmit.edu.au>
2019-04-27 04:26:37 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+end  
   
   def hello
     render html: "hello, world!"
   end
-end
